775288c052dec80a8084b2e65dcaba8cedf3cc3b
hariprasath5106 <hariprasath.10c@gmail.com>
2024-05-09 09:30:01 +0530

    first commit

--- obj/Debug/net8.0/19AI308-Object-Oriented-Programming-using-CSharp--Ex5-Operator-Overloading.AssemblyInfo.cs
+++ obj/Debug/net8.0/19AI308-Object-Oriented-Programming-using-CSharp--Ex5-Operator-Overloading.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("19AI308-Object-Oriented-Programming-using-CSharp--Ex5-Operator-Overloading")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2e1525e3b5c1554315d7097713f17d4530f8673")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf476ad7fc438798e0b820492497a6667e7805d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("19AI308-Object-Oriented-Programming-using-CSharp--Ex5-Operator-Overloading")]
 [assembly: System.Reflection.AssemblyTitleAttribute("19AI308-Object-Oriented-Programming-using-CSharp--Ex5-Operator-Overloading")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
